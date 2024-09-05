2842646fc89b8d9596a149723fd88cc5ccb03c76
Guilherme13041999 <guilherme.s.correia@iesb.edu.br>
2024-09-04 21:56:51 -0300

    Atividades 3 e 4

---
+++ Exercicio-3/exercicio3/src/app/megasena/Page.js
@@ -0,0 +1,8 @@
+import Sorteio from "./Sorteio";
+
+export default function megasena () {
+
+    return(
+        <Sorteio />
+    )
+}
---
+++ Exercicio-3/exercicio3/src/app/megasena/Sorteio.js
@@ -0,0 +1,18 @@
+export default function Sorteio() {
+
+
+
+
+    const numeroAleatorio = Math.floor(Math.random())
+    
+        console.log(numeroAleatorio)
+    
+    
+        return(
+            <>
+                <h2>Número Aleatório</h2>
+                <p>{numeroAleatorio}</p>
+            </>
+        )
+    
+    }
